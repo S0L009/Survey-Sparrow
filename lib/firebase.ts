12243660fd99e89c64c21318e6a3e48a5ccc68c6
Krish <flavoured.waffles@gmail.com>
2025-08-11 02:21:35 +0530

    add firebase creds

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS70EpWTzdUNxz8FigsSS6k8Nwuqbfsvk",
-  authDomain: "surveysparrow-542a6.firebaseapp.com",
-  projectId: "surveysparrow-542a6",
-  storageBucket: "surveysparrow-542a6.firebasestorage.app",
-  messagingSenderId: "682338398728",
-  appId: "1:682338398728:web:d11a961c76856399ac57bd",
-  measurementId: "G-HQ6RHLD9H5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,24 +19,4 @@ const db = getFirestore(app)
 
 
 export { auth, db };
-
-
-
-// lib/firebase.ts
-// import { initializeApp, getApp, getApps } from 'firebase/app'
-// import { getAuth } from 'firebase/auth'
-// import { getFirestore } from 'firebase/firestore'
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCS70EpWTzdUNxz8FigsSS6k8Nwuqbfsvk",
-//   authDomain: "surveysparrow-542a6.firebaseapp.com",
-//   projectId: "surveysparrow-542a6",
-//   storageBucket: "surveysparrow-542a6.appspot.com",
-//   messagingSenderId: "682338398728",
-//   appId: "1:682338398728:web:d11a961c76856399ac57bd",
-//   measurementId: "G-HQ6RHLD9H5"
-// };
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-
-// export const auth = getAuth(app)
 
